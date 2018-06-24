6d53cb311795902dd582e1cdf3103e5818d15d68
longhe.li <lilonghe@outlook.com>
2018-06-24 18:55:58 +0800

    change before connection pool, use one connection

--- config/init.go
+++ config/init.go
@@ -7,15 +7,20 @@ import (
 )
 
 type store struct {
+	db *gorm.DB
 }
 
 func (this *store) Master() *gorm.DB {
+	if this.db != nil {
+		return this.db
+	}
 	db, err := gorm.Open("mysql", "root:lilonghe@tcp(127.0.0.1:3306)/knowledge?loc=Asia%2FShanghai&charset=utf8&parseTime=True")
 	if err != nil {
 		log.Fatal(err)
 	}
+	db.DB().SetMaxOpenConns(2)
+	this.db = db
 
-	db.LogMode(true)
 	return db
 }
 
@@ -29,6 +34,10 @@ func Init() {
 
 func initDataBase() {
 	Store := store{}
-	Store.Master()
-	//db := Store.Master()
+	db := Store.Master().DB()
+	err := db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
 }
